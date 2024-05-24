01f1acd2ca6ea7da8fc30b24cbe858368e44f966
Christopher Deng <chrisdeng@gmail.com>
2024-05-23 20:50:06 -0400

    Modified main.cpp to use the DatabaseManager
    Initialize, open, create tables.

--- main.cpp
+++ main.cpp
@@ -2,10 +2,20 @@
 
 #include <QApplication>
 
+#include "databasemanager.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    DatabaseManager dbManager;
+    if (dbManager.openDatabase("dough.db"))
+    {
+        dbManager.createTables();
+    }
+
     dough w;
     w.show();
+
     return a.exec();
 }
