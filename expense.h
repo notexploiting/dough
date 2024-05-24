f8d93f9de3112c18ca5e9e3ec964f7c0c9f70ceb
Christopher Deng <chrisdeng@gmail.com>
2024-05-23 20:40:01 -0400

    Added Expense and Income header files
    Used these to create a planner for potential functions/variables.

---
+++ expense.h
@@ -0,0 +1,29 @@
+#ifndef EXPENSE_H
+#define EXPENSE_H
+
+#include <QString>
+#include <QDate>
+#include <QSqlDatabase>
+
+class Expense
+{
+public:
+    Expense(const QString& category, const QDate& date, const QString& title, double amount, const QString& description = "");
+
+    bool addToDatabase(QSqlDatabase &db);
+
+    QString getCategory() const;
+    QDate getDate() const;
+    QString getTitle() const;
+    double getAmount() const;
+    QString getDescription() const;
+
+private:
+    QString m_category;
+    QDate m_date;
+    QString m_title;
+    double m_amount;
+    QString m_description;
+};
+
+#endif // EXPENSE_H
---
+++ income.h
@@ -0,0 +1,27 @@
+#ifndef INCOME_H
+#define INCOME_H
+
+#include <QString>
+#include <QDate>
+#include <QSqlDatabase>
+
+class Income
+{
+public:
+    Income(const QDate& date, const QString& title, double amount, const QString& description = "");
+
+    bool addToDatabase(QSqlDatabase &db);
+
+    QDate getDate() const;
+    QString getTitle() const;
+    double getAmount() const;
+    QString getDescription() const;
+
+private:
+    QDate m_date;
+    QString m_title;
+    double m_amount;
+    QString m_description;
+};
+
+#endif // INCOME_H
